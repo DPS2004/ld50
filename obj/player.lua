55177fb94f6972df11f74f1334c09700b52da188
DPS2004 <dpsophinos@gmail.com>
2022-04-02 18:07:24 -0400

    room switching

--- obj/player.lua
+++ obj/player.lua
@@ -76,10 +76,12 @@ function Player:update(dt)
     local xok = true
     local yok = true
     for i,v in ipairs(cs.rooms.c.level.tiles) do
-      local blockhitbox = {x=v.x*8,y=v.y*8,width=8,height=8}
-      if helpers.collide(self.hitbox,blockhitbox) then
-        if helpers.collide(self.oldhitboxx,blockhitbox) then yok = false end
-        if helpers.collide(self.oldhitboxy,blockhitbox) then xok = false end
+      if v.t == 0 then
+        local blockhitbox = {x=v.x*8,y=v.y*8,width=8,height=8}
+        if helpers.collide(self.hitbox,blockhitbox) then
+          if helpers.collide(self.oldhitboxx,blockhitbox) then yok = false end
+          if helpers.collide(self.oldhitboxy,blockhitbox) then xok = false end
+        end
       end
     end
     
@@ -116,16 +118,78 @@ function Player:update(dt)
     self.gunx = (self.gunx + self.gunaimx)/2
     self.guny = (self.guny + self.gunaimy)/2
     
+    ---room transitions
+    if self.x < 8 then
+      self.canmove = false
+      flux.to(self, 30, {x=-9,y=64}):oncomplete(function() 
+        self.x = 119 
+        self.canmove = true 
+        if cs.map[cs.croom].exits.l then
+          cs.croom = cs.map[cs.croom].exits.l
+          cs:updaterooms()
+        end
+      end)
+      flux.to(cs.cube.r, 30, {y=63.5,z=0}):ease('outSine')
+      
+    end
+    
+    if self.x > 120 then
+      self.canmove = false
+      flux.to(self, 30, {x=128+9,y=64}):oncomplete(function() 
+        self.x = 9
+        self.canmove = true 
+        if cs.map[cs.croom].exits.r then
+          cs.croom = cs.map[cs.croom].exits.r
+          cs:updaterooms()
+        end
+      end)
+      flux.to(cs.cube.r, 30, {y=-63.5,z=0}):ease('outSine')
+    end
+    
+    if self.y < 8 then
+      self.canmove = false
+      flux.to(self, 30, {x=64,y=-9}):oncomplete(function() 
+        self.y = 119 
+        self.canmove = true 
+        if cs.map[cs.croom].exits.u then
+          cs.croom = cs.map[cs.croom].exits.u
+          cs:updaterooms()
+        end
+      end)
+      flux.to(cs.cube.r, 30, {y=0,z=-63.5}):ease('outSine')
+      
+    end
+    
+    if self.y > 120 then
+      self.canmove = false
+      flux.to(self, 30, {x=64,y=128+9}):oncomplete(function() 
+        self.y = 9
+        self.canmove = true 
+        if cs.map[cs.croom].exits.d then
+          cs.croom = cs.map[cs.croom].exits.d
+          cs:updaterooms()
+        end
+      end)
+      flux.to(cs.cube.r, 30, {y=0,z=63.5}):ease('outSine')
+    end
+    
+    
+    
+    cs.cube.r.y = (self.x - 64)*-0.5
+    cs.cube.r.z = (self.y - 64)*0.5
+    
+  else
     
   end
   
-  cs.cube.r.y = (self.x - 64)*-0.5
-  cs.cube.r.z = (self.y - 64)*0.5
+  
   
 end
 
-function Player:draw()
+function Player:drawmain(sx,sy)
   color()
+  sx = sx or 0
+  sy = sy or 0
   local flipscale = 1
   local flipoffset = 1
   if self.flip then
@@ -137,9 +201,27 @@ function Player:draw()
     gunflip = -1
   end
   helpers.drawbordered(function(dfx,dfy)
-    ez.drawframe(self.spr,0,self.x+dfx,self.y+dfy,0,1*flipscale,1,9,18)
-    love.graphics.draw(self.gunspr,self.x+self.gunx+dfx,self.y+self.guny+dfy-5,0,gunflip,1,3,3)
+    ez.drawframe(self.spr,0,self.x+dfx+sx,self.y+dfy+sy,0,1*flipscale,1,9,18)
+    love.graphics.draw(self.gunspr,self.x+self.gunx+dfx+sx,self.y+self.guny+dfy-5+sy,0,gunflip,1,3,3)
   end,'white',true)
+end
+
+function Player:draw()
+  
+  self:drawmain(0,0)
+  
+  love.graphics.setCanvas(cs.cube.canvas.l)
+  self:drawmain(128,0)
+  
+  love.graphics.setCanvas(cs.cube.canvas.r)
+  self:drawmain(-128,0)
+  
+  love.graphics.setCanvas(cs.cube.canvas.u)
+  self:drawmain(0,128)
+  
+  love.graphics.setCanvas(cs.cube.canvas.d)
+  self:drawmain(0,-128)
+  
 end
 
 return Player
